69d3adc43e79e9df222b9521d13c50df45aa75e8
emanuelaf <furfaro@MBP-EF.local>
2022-06-05 16:20:39 -0700

    added confidence interval for conditional entropy measures

--- funs.R
+++ funs.R
@@ -1,7 +1,7 @@
 #### Functions
 
 # Conditional Entropy
-# Input: a contingency table
+# Input: a contingency table with response on the columns and independent variable on the rows
 # Returns:
 # 1) rescaled conditional entropy 
 # 2) non rescaled conditional entropy 
@@ -30,11 +30,57 @@ conditional_entropy <- function(x) {
 }
 
 ###########
-# MCE
-###
+# Mutual Conditional Entropy
+###########
 # takes two entropies and calculates their mean
 
 mce <- function(ce1, ce2) {
   mce_value <- (ce1+ce2)/2
   return(mce_value)
 }
+
+################
+# Confidence intervals for conditional entropy measures
+# Input: 
+# 1) x: a contingency table with response on the columns and independent variable on the rows
+# 2) M: number of iterations. Default is 1000
+# entropy_measure: the entropy measure to be calculated, only accepts "ce_rescaled",
+# 3) "ce","marginal_entropy","rowwise_ce". Default is ce_rescaled.
+# 4) conf_level: confidence level. Default is 95%.
+# Returns:
+# confidence interval 
+
+
+ci <- function(x, M=1000, entropy_measure="ce_rescaled", conf_level = 0.95) {
+  if (!(entropy_measure %in% c("ce_rescaled","ce","marginal_entropy","rowwise_ce")))
+  stop("A measure of entropy must be specified")
+  n <- sum(x)
+  mimicked_matrix <- matrix(NA, ncol = ncol(x), nrow =nrow(x))
+  list_mimicked_matrix <- replicate(M, mimicked_matrix, simplify=F)
+  ce_distribution <- numeric(M)
+  ce_distribution_rows <- matrix(NA, ncol = M, nrow =nrow(x))
+  for (r in 1:M) {
+    for (i in 1:nrow(x)) {
+      v_prob = x[i,]/sum(x[i,])
+      n = sum(x[i,])
+      list_mimicked_matrix[[r]][i,] <- rmultinom(1, n, prob = v_prob)
+    }
+    ce_distribution[r] <- 
+      (entropy_measure=="ce_rescaled")*conditional_entropy(list_mimicked_matrix[[r]])[[1]]+
+      (entropy_measure=="ce")*conditional_entropy(list_mimicked_matrix[[r]])[[2]]+
+      (entropy_measure=="marginal_entropy")*conditional_entropy(list_mimicked_matrix[[r]])[[3]]
+    ce_distribution_rows[,r]  <- (entropy_measure=="rowwise_ce")*conditional_entropy(list_mimicked_matrix[[r]])[[4]]
+  }
+  if (entropy_measure != "rowwise_ce") {
+    ci_l <- quantile(ce_distribution, prob = (1-conf_level)/2)
+    ci_u <- quantile(ce_distribution, prob = conf_level+(1-conf_level)/2)
+    return(c(ci_l, ci_u))}
+  else {
+    ci_l <- apply(ce_distribution_rows,1, quantile, prob = (1-conf_level)/2)
+    ci_u <- apply(ce_distribution_rows,1, quantile, prob = conf_level+(1-conf_level)/2)
+    return(return(cbind(ci_l, ci_u)))
+  }
+}
+
+ci(x1, entropy_measure="rowwise_ce")
+
