ec885484416f5ec4bd72e983791a797ec972ce6b
emanuelaf <furfaro@MBP-EF.lan>
2022-06-15 16:18:20 -0400

    minor change

--- rosenberg-data-manipulation.R
+++ rosenberg-data-manipulation.R
@@ -9,7 +9,7 @@ table(adolescents$gender)
 
 adolescents_data_m_us <- adolescents[adolescents$gender == 1,]
 adolescents_data_m_us <- adolescents_data_m_us[(rowSums(adolescents_data_m_us[,1:10] == 0) == 0),] 
-adolescents_data_f_us <- adolescents_data_f_us[(rowSums(adolescents_data_f_us[,1:10] == 0) == 0),] 
 adolescents_data_f_us <- adolescents[adolescents$gender == 2,]
+adolescents_data_f_us <- adolescents_data_f_us[(rowSums(adolescents_data_f_us[,1:10] == 0) == 0),] 
 nrow(adolescents_data_m_us)
 nrow(adolescents_data_f_us)
